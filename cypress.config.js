0ee64d5021e7602fbf6bdf046ad9380176e23e25
leonardo arias <ariasnavarro2496@gmail.com>
2025-04-02 10:59:47 -0500

    02-04-2025

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ady26i',
   e2e: {
     //baseUrl: "http://localhost:3000",  // Puerto al ingresar
     viewportWidth: 1500,
     viewportHeight: 900,
     chromeWebSecurity: false,
     defaultCommandTimeout: 20000,
-    specPattern: "cypress/e2e/features/*.feature",
+    //specPattern: "cypress/e2e/features/*.feature",
   }
 
 
 });
+
